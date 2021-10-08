0ea251b83702730894f9ea4bebb69575eacc6cac
allyssa <allyssa.olan@my.jcu.edu.au>
2021-10-08 12:47:26 +1000

    Add navigation bar

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,11 +1,32 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonButtons, IonContent, IonHeader, IonIcon, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 import Weather from '../components/Weather';
+import { personCircle, helpCircle } from 'ionicons/icons';
 
 const Tab1: React.FC = () => {
   return (
     <IonPage>
+
+      <IonToolbar>
+         <IonTitle>Home</IonTitle>
+          <IonButtons slot="end">
+            <IonButton>
+              Home
+            </IonButton>
+            <IonButton>
+              About
+            </IonButton>
+            <IonButton>
+              News
+            </IonButton>
+            <IonButton>
+              Contact
+            </IonButton>
+          </IonButtons>
+        </IonToolbar>
+
+      
       <IonHeader>
         <IonToolbar>
           <IonTitle>Tab 1</IonTitle>
@@ -20,6 +41,7 @@ const Tab1: React.FC = () => {
         <Weather name="Tab 1 page" />
       </IonContent>
     </IonPage>
+
   );
 };
 
